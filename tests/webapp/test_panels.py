eb9efa305f9e7724b836a8a75cd4fa224a9887f3
Michal Dyzma <mdyzma@gmail.com>
2018-07-09 15:20:30 +0200

    Tests to panels urls

--- tests/webapp/test_panels.py
+++ tests/webapp/test_panels.py
@@ -8,11 +8,15 @@ def test_base_panel_error():
     with pytest.raises(NotImplementedError):
         p.calculate_price()
 
-    # def test_panl_one_price():
-    #     assert True
+def test_panl_one_url():
+    p = PanelOne()
+    assert 'http://time.com', p.url
 
-    # def test_panl_two_price():
-    #     assert True
+def test_panl_two_price():
+    p = PanelTwo()
+    test_url = 'http://openlibrary.org/earch.json?q=the+lord+of+the+rings'
+    assert test_url, p.url
 
-    # def test_panl_three_price():
-    #     assert True
+def test_panl_three_url():
+    p = PanelThree()
+    assert 'http://time.com', p.url
